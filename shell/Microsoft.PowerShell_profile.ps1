f30c198be16e3561d9b6052a146474f05cb8c1bd
b1mind <brent@1mindsolutions.com>
2020-09-28 01:41:57 -0600

    added alias for cfig

--- shell/Microsoft.PowerShell_profile.ps1
+++ shell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,18 @@
-# Must add this to default profile location to load this
+# *** Must add this to default profile location to load this ***
 # $fileContents = [string]::join([environment]::newline, (get-content -path ~/cfig/shell/Microsoft.PowerShell_profile.ps1))
 # invoke-expression $fileContents
 
+# >> Plugins
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-Theme Star # Darkblood # Robbyrussell 
+# Install-Module -Name PSFz
 
-# Settings
+# >> Settings
+Set-Theme Star # Darkblood # Robbyrussell 
 Set-PSReadlineOption -EditMode vi -BellStyle None
 
-# Startup Logo
-$date= Get-Date
+# >> Startup Logo
+$date = Get-Date
 $time = Get-Date -Format "hh:mm tt"
 Write-Host "                                  ---------------- " 
 Write-Host "    __          _             _ " -foregroundColor Blue
@@ -20,6 +22,10 @@ Write-Host "    | |/ /\/\ \| || | | || (_| |  $time" -foregroundColor Blue
 Write-Host "    |_|\/    \/|_||_| |_| \__,_|" -foregroundColor Blue
 Write-Host "                                  -------------------------"
 
+# >> Alias and Vars
 Function wsPath {Set-Location -Path D:\websites\}
 Set-Alias -name ws -value wsPath
+Function configPath {Set-Location -Path ~\cfig\}
+Set-Alias -name cf -value configPath
+
 
